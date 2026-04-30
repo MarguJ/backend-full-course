ac48bfbadbee58c99827ebde582a62609925915b
MarguJ <margulesjoa@gmail.com>
2026-04-30 00:39:09 -0300

    login routes doing

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,10 +11,21 @@ const __filename = fileURLToPath(import.meta.url);
 // Get the directory name of the file path
 const __dirname = dirname(__filename);
 
-// Serving up the HTML file from the /public directory
+// Middleware
+app.use(express.json());
+// Servers the HTML file from the /public directory.
+// Tells express to serve all files from the public folder as static assets / files.
+// Any request for the css files will be resolved to the public directory.
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Serving up the HTML file from the /public directory.
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// Routes
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`);
