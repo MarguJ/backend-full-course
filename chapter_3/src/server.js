670af5a07c8035e2cac8454fb153ae8a07d40b68
MarguJ <margulesjoa@gmail.com>
2026-05-02 00:15:26 -0300

    Lol no funciona el botoncito jaja

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +28,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/auth", authRoutes);
+app.use("/todos", authMiddleware, todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`);
